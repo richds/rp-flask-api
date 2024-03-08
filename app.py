34cf7e940ec54f4989ac0df0dba67b84bd9d9283
Richard Sirokman <richds@gmail.com>
2024-03-08 09:23:16 -0800

    first commit with working API

---
+++ app.py
@@ -0,0 +1,18 @@
+# app.py
+
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+from flask import render_template # Remove: import Flask
+import connexion
+
+app = connexion.App(__name__, specification_dir="./")
+app.add_api("swagger.yml")
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
