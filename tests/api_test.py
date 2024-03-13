d575c4d5c5cecbeabf030657cf4f89f69a2484bc
Richard Sirokman <richds@gmail.com>
2024-03-13 15:36:15 -0700

    made tests work with connexion v2

--- tests/api_test.py
+++ tests/api_test.py
@@ -6,7 +6,7 @@ import json
 base_url = "http://127.0.0.1:8000"
 api_url = base_url + "/api/"
 people_url = api_url + "people/"
-
+note_url = api_url + "notes/"
 mimetype = 'application/json'
 headers = {
     'Content-Type': mimetype,
@@ -31,7 +31,7 @@ def test_hello_world():
 
 
 def test_get_people():
-    r = requests.get(people_url)
+    r = requests.get(people_url.rstrip("/"))
     assert r.status_code == 200
     items_to_check = ("Tooth", "Fairy", "Knecht", "timestamp")
     for item in items_to_check: 
@@ -43,9 +43,9 @@ def test_put_person():
         "fname": "Art",
         "lname": "Vandelay"
     }
-    r = requests.post(url=people_url, headers=headers, json=new_person)
-    assert r.status_code == 201, r.reason
-    r = requests.get(people_url)
+    r = requests.post(url=people_url.rstrip("/"), headers=headers, json=new_person)
+    assert r.status_code == 201, r.text
+    r = requests.get(people_url.rstrip("/"))
     assert r.status_code == 200
     items_to_check = ("Art", "Vandelay")
     for item in items_to_check:
@@ -58,7 +58,7 @@ def test_get_single_person():
         "lname": "Fairy"
     }
     r = requests.get(people_url + myguy["lname"])
-    assert r.status_code == 200, r.url
+    assert r.status_code == 200, r.text
     items_to_check = ("Tooth", "Fairy")
     for item in items_to_check:
         assert item in r.text, item + " not found in return string: " + pretty_print_json(r.text)
@@ -79,8 +79,8 @@ def test_update_person():
         "lname": "Bunny"
     }
     r = requests.put(url=people_url + myguy["lname"], headers=headers, json=myguy)
-    assert r.status_code == 200, r.reason
-    r = requests.get(people_url)
+    assert r.status_code == 201, r.reason
+    r = requests.get(people_url.rstrip("/"))
     assert r.status_code == 200
     items_to_check = ("Barry", "Bunny")
     for item in items_to_check:
